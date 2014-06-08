e99d2dd3870018da35cc8abffc331b27231bc894
Paul Colombo <Paul@Pauls-MacBook-Pro.local>
2014-06-08 01:22:30 -0400

    Iterate over resolutions. Refactor for neatness.

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # Tessel Camera Tester
 ##What is it?
-Takes a photo at each available compresion setting. Based on sample code found here: http://start.tessel.io/modules/camera
+Takes a photo at each available resolution and compresion setting. Based on sample code found here: http://start.tessel.io/modules/camera
 
 Related to forum issue: http://forums.tessel.io/t/garbled-image-from-camera/63
 
 ##Installation
+Assumes you have node and tessel already installed.
 ```.js 
 npm install
 ```
 
-### Running
+## Running
 ```.js
 tessel run app.js --upload-dir .
 ```
 
-### To do
-* Add loop for Resolutions
-* Clean up this ugly-ass code
+## To do
+* ~~Add loop for Resolutions~~
+* ~~Clean up this ugly-ass code~~
--- app.js
+++ app.js
@@ -1,49 +1,73 @@
-var tessel = require('tessel');
-var camera = require('camera-vc0706').use(tessel.port['A'], { resolution: 'vga'});
+var resolutions = ['qqvga','qvga','vga'];
+var currentResolution = 0;
+
 var compressionRanges = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1];
-var compressionIndex = 0;
+var currentCompression = 0;
+
+var tessel = require('tessel');
+var camera = require('camera-vc0706').use(tessel.port['A'], 
+  { 
+    resolution: resolutions[currentResolution],
+    compression: compressionRanges[currentCompression]
+  });
 
 var notificationLED = tessel.led[3]; // Set up an LED to notify when we're taking a picture
 
+var App = {};
 
-// Wait for the camera module to say it's ready
-camera.on('ready', function() {
+App.onCameraReady = function() {
+  // Let's get this party started
+  console.log('---START---');
+  console.log('--Resolution:',resolutions[currentResolution]);
+  console.log('-Compression:',compressionRanges[currentCompression]);
   notificationLED.high();
-  camera.setCompression(compressionRanges[compressionIndex],function(err){
-    if (err) console.log("Error setting compression: ",err);
-  });
-});
+  camera.takePicture(App.onCameraPicture);
+};
 
-
-camera.on('error', function(err) {
+App.onCameraError = function(err){
   console.error(err);
-});
+};
 
-camera.on('compression', function(c) {
-  console.log("Compression set to: "+c);
-  camera.takePicture(function(err,image){
-   if (err) {
-      console.log('error taking image', err);
-    } else {
-      notificationLED.low();
-      var name = 'vga-'+String(compressionRanges[compressionIndex])+'.jpg';
-      console.log('Saving', name, '...');
-      process.sendfile(name, image);
-      console.log('Save successful.');
-      if(compressionIndex < compressionRanges.length-1) { 
-        ++compressionIndex;
-        camera.setCompression(
-          compressionRanges[compressionIndex],
-          function(err){ if(err) console.log('Error setting compression:',err)
-          });
-      } else {
-        console.log("All done.");
-        camera.disable();
-      }
-    }
-  });
-});
+App.onCameraResolution = function(err) {
+  // Reset compression and start taking pictures
+  if (err)  console.error(err);
+  currentCompression = 0;
+  console.log('--Resolution changed to:',resolutions[currentResolution]);
+  camera.setCompression(compressionRanges[currentCompression],App.onCameraCompression);
+};
 
-camera.on('resolution', function(r) {
-  console.log("Resolution set to: "+r);
-});
+App.onCameraCompression = function(err){
+  // Take a picture
+  if (err) console.error(err);
+  console.log('-Compression changed to:',compressionRanges[currentCompression]);
+  camera.takePicture(App.onCameraPicture);
+};
+
+App.onCameraPicture = function(err, pic) {
+  // Save file and update settings
+  if (err)  console.error(err);
+
+  var filename = resolutions[currentResolution]+'-'+String(compressionRanges[currentCompression])+'.jpg';
+  notificationLED.low();
+  
+  console.log('Saving', filename,'...');
+  process.sendfile(filename,pic); // save file to host system
+
+  if(currentCompression < compressionRanges.length-1) {
+    ++ currentCompression;
+    camera.setCompression(compressionRanges[currentCompression],App.onCameraCompression);
+  } else if (currentResolution < resolutions.length-1) {
+    ++ currentResolution;
+    camera.setResolution(resolutions[currentResolution],App.onCameraResolution);
+  } else {
+    camera.disable();
+    console.log('---END---');
+  }
+  
+};
+
+camera.on('ready', App.onCameraReady);
+camera.on('error', App.onCameraError);
+//camera.on('compression', App.onCameraCompression);
+//camera.on('resolution', App.onCameraResolution);
+//camera.on('picture', App.onCameraPicture);
